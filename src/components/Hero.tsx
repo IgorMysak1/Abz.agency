e430e05cab7ee386a9b61fe1e46e281d3f747c71
Your Ihor <igormusak1@gmail.com>
2022-05-07 18:06:32 +0300

    create hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,23 @@
 import React, { FC } from "react";
 import "../style/hero.scss";
+import { Button } from "./Button";
 export const Hero: FC = () => {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <div className="hero__title">Test assignment for front-end developer</div>
+      <div className="hero__subtitle">
+        What defines a good front-end developer is one that has skilled
+        knowledge of HTML, CSS, JS with a vast understanding of User design
+        thinking as they'll be building web interfaces with accessibility in
+        mind. They should also be excited to learn, as the world of Front-End
+        Development keeps evolving.
+      </div>
+      <Button
+        text="Sign Up"
+        handler={() => {
+          console.log("Sign Up");
+        }}
+      />
+    </div>
+  );
 };
