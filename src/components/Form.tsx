a6416e600947fab881039ef6ee264a8a9828407b
Your Ihor <igormusak1@gmail.com>
2022-05-09 12:07:41 +0300

    create registered user alert section

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,11 +3,13 @@ import { Input } from "./Input";
 import { FieldsProperties } from "../constants/fieldsProperties";
 import { RadioButton } from "./RadioButton";
 import { Button } from "./Button";
-import { name, email, phone } from "../utilits/regExpConstants";
+import { name, email, phone } from "../constants/regExpConstants";
 import { ValidFields } from "../constants/ValidFields";
 import "../style/form.scss";
 
 export const Form: FC = () => {
+  const [successfullyRegistered, setSuccessfullyRegistered] =
+    useState<boolean>(false);
   const [formProperties, setFormProperties] = useState<FieldsProperties>({
     name: "",
     phone: "",
@@ -34,12 +36,12 @@ export const Form: FC = () => {
     });
   };
   const onChangeInputFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    const nameFile = e.target.files?.[0]?.name;
+    const fileName = e.target.files?.[0]?.name;
     setFormProperties({
       ...formProperties,
       file: {
-        name: nameFile ?? "Upload your photo",
-        length: nameFile === undefined ? 0 : 1,
+        name: fileName ?? "Upload your photo",
+        length: fileName === undefined ? 0 : 1,
       },
     });
   };
@@ -57,67 +59,70 @@ export const Form: FC = () => {
     };
     setIsValidFields(validProperties);
     if (!Object.values(validProperties).every(Boolean)) return;
-    setFormProperties({
-      name: "",
-      phone: "",
-      email: "",
-      job: "Frontend developer",
-      file: {
-        name: "Upload your photo",
-        length: 0,
-      },
-    });
+    setSuccessfullyRegistered(true);
   };
   return (
     <form className="form">
-      <div className="form__inputs">
-        <Input
-          valid={isValidFields.validName}
-          placeholder="name"
-          hint="John"
-          formProperties={formProperties}
-          onChangeInputValue={onChangeInputValue}
-        />
-        <Input
-          valid={isValidFields.validEmail}
-          placeholder="email"
-          hint="example@gmail.com"
-          formProperties={formProperties}
-          onChangeInputValue={onChangeInputValue}
-        />
-        <Input
-          valid={isValidFields.validPhone}
-          placeholder="phone"
-          hint="+38 (XXX) XXX - XX - XX"
-          formProperties={formProperties}
-          onChangeInputValue={onChangeInputValue}
-        />
-      </div>
-      <div className="form__radio radio-form">
-        <h1 className="radio-form__title">Select your position</h1>
-        <RadioButton
-          defaultChecked
-          text="Frontend developer"
-          onChangeRadioButton={onChangeRadioButton}
-        />
-        <RadioButton
-          text="Backend developer"
-          onChangeRadioButton={onChangeRadioButton}
-        />
-        <RadioButton
-          text="Designer"
-          onChangeRadioButton={onChangeRadioButton}
-        />
-        <RadioButton text="QA" onChangeRadioButton={onChangeRadioButton} />
-      </div>
-      <div
-        className={`form__photo ${!isValidFields.validFile ? "invalid" : ""}`}
-      >
-        <label htmlFor="files">Upload</label>
-        <span>{formProperties.file.name}</span>
-        <input id="files" type="file" onChange={onChangeInputFile}></input>
-      </div>
-      <Button text="Sign Up" handler={submitForm} />
+      {successfullyRegistered ? (
+        <div className="form__registered">
+          <h1>User successfully registered</h1>
+          <img src="img/success-registered.jpg" alt="Successfully registered" />
+        </div>
+      ) : (
+        <div className="form__unregistered">
+          <h1 className="form__title">Working with POST request</h1>
+          <div className="form__inputs">
+            <Input
+              valid={isValidFields.validName}
+              placeholder="name"
+              hint="John"
+              formProperties={formProperties}
+              onChangeInputValue={onChangeInputValue}
+            />
+            <Input
+              valid={isValidFields.validEmail}
+              placeholder="email"
+              hint="example@gmail.com"
+              formProperties={formProperties}
+              onChangeInputValue={onChangeInputValue}
+            />
+            <Input
+              valid={isValidFields.validPhone}
+              placeholder="phone"
+              hint="+38 (XXX) XXX - XX - XX"
+              formProperties={formProperties}
+              onChangeInputValue={onChangeInputValue}
+            />
+          </div>
+          <div className="form__radio radio-form">
+            <h1 className="radio-form__title">Select your position</h1>
+            <RadioButton
+              defaultChecked
+              text="Frontend developer"
+              onChangeRadioButton={onChangeRadioButton}
+            />
+            <RadioButton
+              text="Backend developer"
+              onChangeRadioButton={onChangeRadioButton}
+            />
+            <RadioButton
+              text="Designer"
+              onChangeRadioButton={onChangeRadioButton}
+            />
+            <RadioButton text="QA" onChangeRadioButton={onChangeRadioButton} />
+          </div>
+          <div
+            className={`form__photo ${
+              !isValidFields.validFile ? "invalid" : ""
+            }`}
+          >
+            <label htmlFor="files">Upload</label>
+            <span>{formProperties.file.name}</span>
+            <input id="files" type="file" onChange={onChangeInputFile}></input>
+          </div>
+          <Button text="Sign Up" handler={submitForm} />
+        </div>
+      )}
     </form>
   );
 };
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -6,9 +6,7 @@ import { Form } from "./Form";
 export const SignUp: FC = () => {
   return (
     <div className="signUp">
-      <h1 className="signUp__title">Working with POST request</h1>
       <Form />
-      {/* <Button text="Sign up" handler={() => console.log("Sign up")} /> */}
     </div>
   );
 };
